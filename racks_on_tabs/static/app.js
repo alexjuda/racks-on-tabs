a27bf053c0d56b54ab92a6a958576304f52d1e64
Alexander Juda <alexanderjuda@gmail.com>
2020-11-11 17:57:01 +0100

    Add scroll detection poc

--- racks_on_tabs/static/app.js
+++ racks_on_tabs/static/app.js
@@ -1,2 +1,9 @@
 (function () {
+    window.addEventListener('load', _evt => {
+        const obs = new IntersectionObserver(entries => {
+            console.log(entries);
+            document.querySelector('#debug-p').textContent = (entries[0].isIntersecting ? "intersecting": "not intersecting");
+        }, {});
+        obs.observe(document.querySelector('#post-table-p'))
+    });
 })();
