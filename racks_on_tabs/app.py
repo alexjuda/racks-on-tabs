3919ce4df6283edc25137dae95aa0c552e4a1661
Alexander Juda <alexanderjuda@gmail.com>
2020-11-11 19:22:37 +0100

    Implement infinite scroll in batches of 10

--- racks_on_tabs/app.py
+++ racks_on_tabs/app.py
@@ -13,30 +13,34 @@ def _make_app(csv_path):
 
     @app.route('/')
     def _index():
-        csv_doc = _read_csv_doc(csv_path)
+        csv_doc = _read_csv_doc(csv_path, 0, -1)
         return flask.render_template('index.html',
                                      csv_path=csv_path,
-                                     n_rows=len(csv_doc['rows']),
+                                     n_rows=csv_doc['n_all_rows'],
                                      n_cols=len(csv_doc['col_names']),
                                      table_headers=csv_doc['col_names'],
                                      app_js_url=_url_for('static', filename='app.js'))
 
     @app.route('/partial/rows')
     def _partial_rows():
-        csv_doc = _read_csv_doc(csv_path)
+        n_rows = int(flask.request.args.get('n_rows', 10))
+        after_id = int(flask.request.args.get('after_id', -1))
+        csv_doc = _read_csv_doc(csv_path, n_rows, after_id)
         return flask.render_template('table_rows.html',
-                                     rows_values=[[row[col]
-                                                   for col in csv_doc['col_names']]
-                                                  for row in csv_doc['rows']])
+                                     rows_values=[(row_id, [row[col]
+                                                            for col in csv_doc['col_names']])
+                                                  for row_id, row in csv_doc['rows']])
 
     return app
 
 
-def _read_csv_doc(path):
+def _read_csv_doc(path, n_rows, after_id):
     with open(path) as f:
         reader = csv.DictReader(f)
+        all_rows = list(reader)
 
-        return {'rows': list(reader),
+        return {'rows': list(enumerate(all_rows))[after_id + 1:after_id + 1 + n_rows],
+                'n_all_rows': len(all_rows),
                 'col_names': reader.fieldnames}
 
 
--- racks_on_tabs/static/app.js
+++ racks_on_tabs/static/app.js
@@ -1,12 +1,40 @@
 (function () {
     // app state
-    window.racksOnTabs = {
-    };
+    window.racksOnTabs = {}
+
+    function readAppState(key) {
+        return window.racksOnTabs[key];
+    }
+
+    function writeAppState(key, value) {
+        window.racksOnTabs[key] = value;
+    }
 
     function shouldFetchRows(interactionEntries) {
         console.assert(interactionEntries.length === 1);
         return interactionEntries[0].isIntersecting;
     }
+
+    function maxByFn(coll, fn) {
+        let maxValSoFar = null;
+        let maxElementSoFar = null;
+        coll.forEach(element => {
+            if (maxValSoFar === null || fn(element) > maxValSoFar) {
+                maxValSoFar = fn(element);
+                maxElementSoFar = element;
+            }
+        });
+        return maxElementSoFar;
+    }
+
+    function maxCurrentRowId() {
+        const rowIds = [...document.querySelectorAll('.csv-table-row')]
+              .map(e => { return e.getAttribute('data-row-id')});
+        const maxRowId = Math.max(...rowIds);
+        return (isFinite(maxRowId) ? maxRowId : -1);
+    }
+
+    const csvRowBatchSize = 10;
 
     window.addEventListener('load', _evt => {
         const obs = new IntersectionObserver(entries => {
@@ -14,7 +42,8 @@
             document.querySelector('#debug-p').textContent = (entries[0].isIntersecting ? "intersecting": "not intersecting");
 
             if (shouldFetchRows(entries)) {
-                fetch('/partial/rows', {})
+                const maxId = maxCurrentRowId();
+                fetch(`/partial/rows?n_rows=${csvRowBatchSize}&after_id=${maxId}`, {})
                     .then(resp => {
                         return resp.text();
                     })
